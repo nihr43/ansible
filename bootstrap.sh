88b924552809f0d7aec031e489b2c54288be6d1f
Your Name <you@example.com>
2018-04-09 06:25:17 +0000

    dr

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,17 +4,27 @@
 # bsd clients depend python27
 # ansible hosts file needs to spec py27 as well
 
-# here we ssh into hypervisor, grab mac address of guest, ssh into guest, set correct hostname
+
+TMP_DIR=`mktemp -d`
 
 
-UUID=$(ssh -o StrictHostKeyChecking=no 192.168.1.100 -- vm info $1 | grep "fixed-mac-address" | awk '{print $2}' | awk -F ':' '{print $4$5$6}' | head -n1)
+# here we ssh into hypervisor, grab mac address of guest
+
+UUID=$(ssh -o StrictHostKeyChecking=no 192.168.1.100 -p 2222 -- vm info $1 | grep "fixed-mac-address" | awk '{print $2}' | awk -F ':' '{print $4$5$6}' | head -n1)
 UUID=$(echo -n $UUID".localdomain")
+
+
+# install python for ansible
+
 ssh -o StrictHostKeyChecking=no $UUID -- pkg install -y python27
 
-scp $UUID:/etc/rc.conf /tmp/rc.conf
-sed -i -e "s|hostname=.*|hostname=\"$1\"|" /tmp/rc.conf
-scp /tmp/rc.conf $UUID:/etc/rc.conf
+
+# grab rc.conf, modify it locally, send it back
+
+scp $UUID:/etc/rc.conf $TMP_DIR/rc.conf
+sed -i -e "s|hostname=.*|hostname=\"$1\"|" $TMP_DIR/rc.conf
+scp $TMP_DIR/rc.conf $UUID:/etc/rc.conf
+
 
 ssh $UUID reboot
-
-rm /tmp/rc.conf
+rm -rf $TMP_DIR
