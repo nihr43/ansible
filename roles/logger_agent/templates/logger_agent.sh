c79f110897003c8ef8756b3e966d4225a794f43b
Your Name <you@example.com>
2019-01-06 14:03:59 -0600

    8de48c

--- roles/logger_agent/templates/logger_agent.sh
+++ roles/logger_agent/templates/logger_agent.sh
@@ -6,17 +6,18 @@ sleep `jot -r 1 0 1800`
 
 NODES="{{ logger_nodes }}"
 DATE=`date +%s`
+KEY="/root/keys/id_ed25519"
 
 # select a random logger node to copy our logs to
 TARGET=`echo $NODES | tr ' ' '\n' | shuf | head -n1`
 
 # make our target dir if not exists
-ssh $TARGET -- "[ -e /opt/log/`hostname -s` ] || mkdir -p /opt/log/`hostname -s`"
+ssh -i $KEY logger@$TARGET -- "[ -e /opt/log/`hostname -s` ] || mkdir -p /opt/log/`hostname -s`"
 
 set -e
 
 # copy and truncate logs
 cd /var/log
-find . -type f -exec scp {} $TARGET:/opt/log/`hostname -s`/{}.$DATE \; && {
+find . -type f -exec scp -i $KEY {} logger@$TARGET:/opt/log/`hostname -s`/{}.$DATE \; && {
   find . -type f -exec truncate -s 0 {} \;
 }
