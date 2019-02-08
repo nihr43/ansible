c219745e09d0b6586c062024dcf2b8f53e124d76
Your Name <you@example.com>
2019-02-07 20:55:19 -0600

    9b881a

--- roles/etcd/templates/start_etcd.sh
+++ roles/etcd/templates/start_etcd.sh
@@ -1,7 +1,7 @@
 etcd --name {{ ansible_hostname }} --initial-advertise-peer-urls http://{{ ansible_hostname }}:2380 \
   --listen-peer-urls http://0.0.0.0:2380 \
-  --listen-client-urls http://0.0.0.0:2379,http://127.0.0.1:2379 \
-  --advertise-client-urls http://0.0.0.0:2379 \
+  --listen-client-urls http://{{ ansible_hostname }}:2379,http://127.0.0.1:2379 \
+  --advertise-client-urls http://{{ ansible_hostname }}:2379 \
   --initial-cluster-token 38ae8726-8e20-43fe-9605-5fe97a347c24 \
   --initial-cluster {% for node in groups.etcd_nodes %}{{ node }}=http://{{ node }}:2380{% if not loop.last %},{% endif %}{% endfor %} \
   --initial-cluster-state new
